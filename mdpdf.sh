7e8a68a1a3991cb565a0440cce2d59a8630e524d
kribesk <kribesk@gmail.com>
2015-06-01 14:09:08 +0300

    added pdf generation to make: make docs

--- mdpdf.sh
+++ mdpdf.sh
@@ -1,4 +1,4 @@
 input=$1
 output=$(echo $1 | sed 's/\..*$/.pdf/')
 
-gpp -H $input | pandoc -f markdown -t latex -o $output
+gpp -H $input | pandoc -f markdown -t latex -o $output -V geometry:margin=1in
