da312699e678142b4214451b2e35f8cc6194eb89
= <=>
2015-06-06 14:32:45 +0300

    Updated 2 tests for merge sort

--- sorting/merge_sort.h
+++ sorting/merge_sort.h
@@ -2,8 +2,6 @@
 
 typedef std::vector<int> vi;
 
-const int N = 1e4;
-
 vi merge(const vi &lm, const vi &mr);
 
 vi merge_sort(const vi &a, const int l, const int r);
--- sorting/test_merge_sort.cpp
+++ sorting/test_merge_sort.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #define forn(i, n) for (int i = 0; i < n; ++i)
 
 const int M = 100; // number of check iterations
+const int N = 1e4; // size of the array
 
 TEST(TestMergeSort, SortTest) {
 	/*int n;
@@ -20,8 +21,8 @@ TEST(TestMergeSort, SortTest) {
 	forn(i, n)
 		printf("%d ", b[i]);
         */
-	srand ( unsigned ( time(0) ) );
-  	vi a(N), b;
+  srand ( unsigned ( time(0) ) );
+  vi a(N), b;
 	forn(i, N)
 		a[i] = i; 
 	forn(_, M) {
@@ -31,4 +32,36 @@ TEST(TestMergeSort, SortTest) {
 		forn(i, N)
 			ASSERT_TRUE(a[i] == i);
 	}
+}
+
+TEST(TestMergeSort, SortTest2) {
+  vi a(N), b;
+  const int K = 10;
+  forn(k, K) { 
+    forn(i, N)
+      a[i] = i & ((1 << k) - 1);
+    forn(_, M) {
+      random_shuffle(a.begin(), a.end());
+      b = merge_sort(a, 0, N - 1);
+      std::sort(a.begin(), a.end());
+      forn(i, N)
+        ASSERT_TRUE(a[i] == b[i]); 
+    }
+  }
+}
+
+TEST(TestMergeSort, SortTest3) {
+  vi a(N), b;
+  const int K = 10;
+  forn(k, K) { 
+    forn(i, N)
+      a[i] =  i & ((1 << k) - 1) * (i & 1 ? -1 : 1);
+    forn(_, M) {
+      random_shuffle(a.begin(), a.end());
+      b = merge_sort(a, 0, N - 1);
+      std::sort(a.begin(), a.end());
+      forn(i, N)
+        ASSERT_TRUE(a[i] == b[i]); 
+    }
+  }
 }
