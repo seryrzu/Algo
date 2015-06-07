bcbd72c0241f4346016c5dcea0d302795fa06b8a
= <=>
2015-06-07 15:45:28 +0300

    Comment about random_shuffle()

--- sorting/quicksort.h
+++ sorting/quicksort.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// it is better to use random_shuffle() before quicksort(). With very high probability after rs the qs will work O(nlogn)
+
 int partition(int a[], int l, int r);
 void quicksort(int a[], int l, int r);
 
